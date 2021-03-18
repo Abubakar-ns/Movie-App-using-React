3ecb35f43eb036751b2138b98e758177336e1d0b
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-19 00:27:18 +0530

    Setting up Folder

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Movie App</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
