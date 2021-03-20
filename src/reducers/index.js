701e64b236ca57aa8ed333afd7cc6385411655fa
Abubakar Aftab <abubakar.aftab786@gmail.com>
2021-03-20 15:52:35 +0530

    Add reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+//always return the new state
+export default function movies(state=[],action){
+    //state same rehra we are not changing the state
+   if(action.type=='ADD_MOVIES'){
+       return action.movies;
+   } 
+
+   return state;
+}
+//UI se add movies ka option woh action par bhejega action
+//[m1,m2] movie add karega aur reducer par bhejega woh ye new 
+//state store ko bhejega jo usey add kardega db mai orginial
+//state ke saath
